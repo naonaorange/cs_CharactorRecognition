3ddf30d47f7b31a77769c70c331fa4425a32e167
naonaorange <nao.lunalovegood@gmail.com>
2018-03-12 19:59:27 +0900

    implement OCR sample

--- CharacterRecognition/Program.cs
+++ CharacterRecognition/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using OpenCvSharp;
+using OpenCvSharp.Text;
 
 namespace CharacterRecognition
 {
@@ -13,7 +14,30 @@ namespace CharacterRecognition
         static void Main(string[] args)
         {
             var img = new Mat(@"image.png");
-            Cv2.ImShow("test", img);
+            var gray = new Mat();
+
+            Cv2.CvtColor(img, gray, ColorConversionCodes.BGR2GRAY);
+
+            var ocr = OCRTesseract.Create("./tessdata", "eng");
+            string text;
+            Rect[] boxes;
+            string[] words;
+            float[] confidences;
+            
+
+
+            ocr.Run(gray, out text, out boxes, out words, out confidences);
+
+            Console.WriteLine("text: " + text);
+            for(int i = 0; i < boxes.Count(); i++)
+            {
+                Console.WriteLine("boxes: " + boxes[i]);
+                Console.WriteLine("words: " + words[i]);
+                Console.WriteLine("confidences: " + confidences[i]);
+            }
+
+
+            Cv2.ImShow("test", gray);
             Cv2.WaitKey();
         }
     }
